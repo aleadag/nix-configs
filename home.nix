c5f355abd914da5c0cd2eb10bf887bbca221b312
Alexander Wang <aleadag@gmail.com>
2023-05-29 16:07:28 +0800

    fix(git): update user email config

--- home.nix
+++ home.nix
@@ -166,7 +166,7 @@ in {
     git = {
       enable = true;
       userName = "Alexander Wang";
-      userEmail = "alexander@tiwater.com";
+      userEmail = "aleadag@gmail.com";
       aliases = {
         hist =
           "log --pretty=format:'%C(yellow)[%ad]%C(reset) %C(green)[%h]%C(reset) | %C(red)%s %C(bold red){{%an}}%C(reset) %C(blue)%d%C(reset)' --graph --date=short";
