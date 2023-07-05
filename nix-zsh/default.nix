16b3314e055c670491ebed784538818a54db1c9a
Alexander Wang <aleadag@gmail.com>
2023-07-05 12:29:23 +0800

    fix: nvim and home-manager warnings

--- nix-zsh/default.nix
+++ nix-zsh/default.nix
@@ -19,7 +19,7 @@ in {
     enable = true;
     enableAutosuggestions = true;
     enableCompletion = true;
-    enableSyntaxHighlighting = true;
+    syntaxHighlighting.enable = true;
     initExtra = ''
       # The powerlevel theme I'm using is distgusting in TTY, let's default
       # to something else
