17d2b95a84fec0a73f0c357b3d74713750ddb6a1
Alexander Wang <aleadag@gmail.com>
2024-01-31 19:27:49 +0800

    document tide configure

--- fish.nix
+++ fish.nix
@@ -20,6 +20,7 @@ in
     };
 
     plugins = [
+      # tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time=No --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='Two lines, character' --prompt_connection=Disconnected --powerline_right_prompt_frame=Yes --prompt_connection_andor_frame_color=Lightest --prompt_spacing=Compact --icons='Few icons' --transient=Yes
       { name = "tide"; src = pkgs.fishPlugins.tide.src; }
       {
         name = "plugin-proxy";
