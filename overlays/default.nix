d4e28d1ec9aadd04f7efd6c899ccc9f291982201
Alexander Wang <aleadag@gmail.com>
2024-02-12 03:29:50 +0800

    add missing packages

--- overlays/default.nix
+++ overlays/default.nix
@@ -8,32 +8,22 @@ final: prev:
     (import ../lib { lib = finalLib; pkgs = final; })
   );
 
-  # gaming = flake.inputs.nix-gaming.packages.${prev.system};
-
-  # wallpapers = prev.callPackage ../packages/wallpapers { };
+  wallpapers = prev.callPackage ../packages/wallpapers { };
 
   # # custom packages
   # arandr = prev.arandr.overrideAttrs (_: { src = flake.inputs.arandr; });
 
-  # anime4k = prev.callPackage ../packages/anime4k { };
+  anime4k = prev.callPackage ../packages/anime4k { };
 
-  # change-res = prev.callPackage ../packages/change-res { };
+  change-res = prev.callPackage ../packages/change-res { };
 
-  # inherit (flake.inputs.home-manager.packages.${prev.system}) home-manager;
+  inherit (flake.inputs.home-manager.packages.${prev.system}) home-manager;
 
-  # open-browser = prev.callPackage ../packages/open-browser { };
+  open-browser = prev.callPackage ../packages/open-browser { };
 
   nix-cleanup = prev.callPackage ../packages/nix-cleanup { };
 
-  # nixos-cleanup = prev.callPackage ../packages/nix-cleanup {
-  #   isNixOS = true;
-  # };
-
   nix-whereis = prev.callPackage ../packages/nix-whereis { };
 
-  # nom-rebuild = prev.callPackage ../packages/nom-rebuild { };
-
-  # run-bg-alias = name: command: prev.callPackage ../packages/run-bg-alias { inherit name command; };
-
-  # inherit (flake.inputs.twenty-twenty-twenty.packages.${prev.system}) twenty-twenty-twenty;
+  run-bg-alias = name: command: prev.callPackage ../packages/run-bg-alias { inherit name command; };
 }
