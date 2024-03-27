3f6f1602443f5b64e73141603c7f8016f1e8785c
Alexander Wang <aleadag@gmail.com>
2024-03-27 08:13:36 +0800

    fix(hm/calibre): disable for now as it cannot compile

--- home-manager/desktop/default.nix
+++ home-manager/desktop/default.nix
@@ -1,6 +1,9 @@
-{ config, pkgs, lib, ... }:
-
 {
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
   imports = [
     ./chromium.nix
     ./dunst.nix
@@ -42,7 +45,7 @@
   config = lib.mkIf config.home-manager.desktop.enable {
     i18n.inputMethod = {
       enabled = "fcitx5";
-      fcitx5.addons = with pkgs; [ fcitx5-chinese-addons ];
+      fcitx5.addons = with pkgs; [fcitx5-chinese-addons];
     };
 
     home = {
@@ -52,8 +55,9 @@
       packages = with pkgs; [
         android-file-transfer
         audacious
-        (calibre.override { unrarSupport = true; })
-        (cinnamon.nemo-with-extensions.override { extensions = with cinnamon; [ nemo-fileroller ]; })
+        # Disable for now as it cannot compile
+        # (calibre.override { unrarSupport = true; })
+        (cinnamon.nemo-with-extensions.override {extensions = with cinnamon; [nemo-fileroller];})
         desktop-file-utils
         ffmpeg
         gammastep
@@ -65,7 +69,7 @@
         inkscape
         libreoffice-fresh
         open-browser
-        (mcomix.override { unrarSupport = true; })
+        (mcomix.override {unrarSupport = true;})
         pamixer
         pavucontrol
         playerctl
