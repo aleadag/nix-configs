e7b900f25eb7d70dd391a96c4507e2a6829b007b
Alexander Wang <aleadag@gmail.com>
2024-06-07 11:39:59 +0800

    hm/zellij: update configs

--- home-manager/cli/default.nix
+++ home-manager/cli/default.nix
@@ -88,6 +88,16 @@ in
     ];
 
     programs = {
+      bash = {
+        enable = true;
+        initExtra = /* bash */''
+          if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+          then
+            shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+            exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+          fi
+        '';
+      };
       bat = {
         enable = true;
         themes = {
