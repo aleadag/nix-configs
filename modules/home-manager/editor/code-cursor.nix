b5c2ba757831b49626ae560a83bb706796c348b1
Alexander Wang <aleadag@gmail.com>
2025-02-24 00:07:33 +0800

    hm/cursor: only install the package for now

--- modules/home-manager/editor/code-cursor.nix
+++ modules/home-manager/editor/code-cursor.nix
@@ -10,25 +10,28 @@
   };
 
   config = lib.mkIf config.home-manager.editor.code-cursor.enable {
-    programs.vscode = {
-      enable = true;
-      package = pkgs.code-cursor;
-      # Cannot set extension for now because of extensionDir is unknown!
-      # extensions = with pkgs.vscode-extensions; [
-      #   # General
-      #   vscodevim.vim # Vim (https://marketplace.visualstudio.com/items?itemName=vscodevim.vim)
-      #   arrterian.nix-env-selector # Nix Env Selector (https://marketplace.visualstudio.com/items?itemName=arrterian.nix-env-selector)
-      #
-      #   # Git
-      #   github.vscode-pull-request-github # GitHub Pull Requests (https://marketplace.visualstudio.com/items?itemName=GitHub.vscode-pull-request-github)
-      #
-      #   # Languages
-      #
-      #   # Frontend
-      #
-      #   # Themes
-      #   catppuccin.catppuccin-vsc # Catppuccin theme (https://marketplace.visualstudio.com/items?itemName=Catppuccin.catppuccin-vsc)
-      # ];
-    };
+    home.packages = [ pkgs.code-cursor ];
+
+    # Waiting for PR to be merged:
+    # https://github.com/nix-community/home-manager/pull/6417
+    # programs.vscode = {
+    #   enable = true;
+    #   package = pkgs.code-cursor;
+    #   extensions = with pkgs.vscode-extensions; [
+    #     # General
+    #     vscodevim.vim # Vim (https://marketplace.visualstudio.com/items?itemName=vscodevim.vim)
+    #     arrterian.nix-env-selector # Nix Env Selector (https://marketplace.visualstudio.com/items?itemName=arrterian.nix-env-selector)
+    #
+    #     # Git
+    #     github.vscode-pull-request-github # GitHub Pull Requests (https://marketplace.visualstudio.com/items?itemName=GitHub.vscode-pull-request-github)
+    #
+    #     # Languages
+    #
+    #     # Frontend
+    #
+    #     # Themes
+    #     catppuccin.catppuccin-vsc # Catppuccin theme (https://marketplace.visualstudio.com/items?itemName=Catppuccin.catppuccin-vsc)
+    #   ];
+    # };
   };
 }
