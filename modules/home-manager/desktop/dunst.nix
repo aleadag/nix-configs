1dcd1324417e7f819f26cbf10987bc8af782fd5b
Alexander Wang <aleadag@gmail.com>
2025-02-26 00:02:57 +0800

    hm/dunst: remove theme settings

--- modules/home-manager/desktop/dunst.nix
+++ modules/home-manager/desktop/dunst.nix
@@ -21,54 +21,44 @@
       iconTheme = with config.gtk.iconTheme; {
         inherit name package;
       };
-      settings =
-        with config.home-manager.desktop.theme.colors;
-        let
-          theme = {
-            background = base;
-            foreground = text;
-          };
-        in
-        {
-          global = with config.home-manager.desktop.theme.fonts; {
-            font = "${gui.name} 8";
-            markup = true;
-            format = "<b>%s</b>\\n%b";
-            sort = true;
-            indicate_hidden = true;
-            alignment = "left";
-            show_age_threshold = 60;
-            word_wrap = true;
-            ignore_newline = false;
-            width = 250;
-            height = 200;
-            origin = "top-right";
-            notification_limit = 5;
-            transparency = 0;
-            idle_threshold = 120;
-            follow = "mouse";
-            sticky_history = true;
-            line_height = 0;
-            padding = 8;
-            horizontal_padding = 8;
-            separator_color = surface1;
-            frame_width = 1;
-            frame_color = mantle;
-            show_indicators = false;
-            icon_position = "left";
-            min_icon_size = 48;
-            max_icon_size = 48;
-          };
-          urgency_low = {
-            timeout = 5;
-          } // theme;
-          urgency_normal = {
-            timeout = 10;
-          } // theme;
-          urgency_high = {
-            timeout = 20;
-          } // theme;
+      settings = {
+        global = with config.home-manager.desktop.theme.fonts; {
+          font = "${gui.name} 8";
+          markup = true;
+          format = "<b>%s</b>\\n%b";
+          sort = true;
+          indicate_hidden = true;
+          alignment = "left";
+          show_age_threshold = 60;
+          word_wrap = true;
+          ignore_newline = false;
+          width = 250;
+          height = 200;
+          origin = "top-right";
+          notification_limit = 5;
+          transparency = 0;
+          idle_threshold = 120;
+          follow = "mouse";
+          sticky_history = true;
+          line_height = 0;
+          padding = 8;
+          horizontal_padding = 8;
+          frame_width = 1;
+          show_indicators = false;
+          icon_position = "left";
+          min_icon_size = 48;
+          max_icon_size = 48;
         };
+        urgency_low = {
+          timeout = 5;
+        };
+        urgency_normal = {
+          timeout = 10;
+        };
+        urgency_high = {
+          timeout = 20;
+        };
+      };
     };
   };
 }
