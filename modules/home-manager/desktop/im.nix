4d4fbad113499840ba1dc97f8c04d1a1b5286b2f
Alexander Wang <aleadag@gmail.com>
2025-04-25 21:20:59 +0800

    hm/kitty: set kitty mod

--- modules/home-manager/desktop/im.nix
+++ modules/home-manager/desktop/im.nix
@@ -69,16 +69,6 @@ in
               PreloadInputMethod = true;
             };
           };
-          inputMethod = {
-            GroupOrder."0" = "Default";
-            "Groups/0" = {
-              Name = "Default";
-              "Default Layout" = "us";
-              DefaultIM = "pinyin";
-            };
-            "Groups/0/Items/0".Name = "keyboard-us";
-            "Groups/0/Items/1".Name = "pinyin";
-          };
         };
       };
     };
