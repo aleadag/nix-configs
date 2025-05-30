351d47ac012292518a21fc938831faf4c25ca059
Alexander Wang <aleadag@gmail.com>
2025-05-30 10:57:06 +0800

    hm/syncthing: update settings

--- modules/home-manager/syncthing.nix
+++ modules/home-manager/syncthing.nix
@@ -12,7 +12,34 @@ in
     default = true;
   };
 
-  config = lib.mkIf cfg.enable {
-    services.syncthing.enable = true;
-  };
+  config = lib.mkIf cfg.enable (
+    let
+      devices = {
+        mbx = {
+          id = "SBCCPEE-3TV6WBK-TBSXAQD-6JCZQ6E-JKUJ7OH-QZL462Y-GRAEYZE-FATVIQ2";
+        };
+      };
+      allDevices = builtins.attrNames devices;
+    in
+    {
+      services.syncthing = {
+        enable = true;
+        settings = {
+          inherit devices;
+          folders = {
+            sync = {
+              path = "~/sync";
+              devices = allDevices;
+              ignorePerms = true;
+              copyOwnershipFromParent = true;
+            };
+          };
+          options = {
+            relaysEnabled = true;
+            localAnnounceEnabled = true;
+          };
+        };
+      };
+    }
+  );
 }
