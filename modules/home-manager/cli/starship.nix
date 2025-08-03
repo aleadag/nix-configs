4f8e222d4b341c4a12707f658865246da580b80d
Alexander Wang <aleadag@gmail.com>
2025-08-03 22:52:20 +0800

    🎨 style: format Nix code for consistency across configuration files

--- modules/home-manager/cli/starship.nix
+++ modules/home-manager/cli/starship.nix
@@ -26,7 +26,7 @@
         custom.jj = {
           ignore_timeout = true;
           description = "The current jj status";
-          detect_folders = [".jj"];
+          detect_folders = [ ".jj" ];
           symbol = "🥋 ";
           command = ''
             jj log --revisions @ --no-graph --ignore-working-copy --color always --limit 1 --template '
