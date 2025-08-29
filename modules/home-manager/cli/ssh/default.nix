7baef0438669d79fd47a2e876a6852d8257fb784
Alexander Wang <aleadag@gmail.com>
2025-08-29 11:06:23 +0800

    🔧 chore: fix deprecated SSH configuration options
    Move SSH options from global to matchBlocks to resolve deprecation warnings:
    - Move addKeysToAgent, compression, forwardAgent to matchBlocks.*
    - Move serverAliveCountMax, serverAliveInterval to matchBlocks.*  
    - Set enableDefaultConfig = false to silence future deprecation warning

--- modules/home-manager/cli/ssh/default.nix
+++ modules/home-manager/cli/ssh/default.nix
@@ -16,18 +16,22 @@
     programs = {
       ssh = {
         enable = true;
-        addKeysToAgent = "yes";
-        compression = true;
-        forwardAgent = true;
-        serverAliveCountMax = 2;
-        serverAliveInterval = 300;
+        enableDefaultConfig = false;
+
         includes = [ "local.d/*" ];
+
         extraConfig = lib.optionalString pkgs.stdenv.isDarwin ''
           IgnoreUnknown UseKeychain
           UseKeychain yes
         '';
+
         matchBlocks = {
           "*" = {
+            addKeysToAgent = "yes";
+            compression = true;
+            forwardAgent = true;
+            serverAliveCountMax = 2;
+            serverAliveInterval = 300;
             sendEnv = [ "COLORTERM" ];
           };
           "github.com" = {
