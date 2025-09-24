9ec8c603854552ae6cf26ea296b1671996e1acce
Alexander Wang <aleadag@gmail.com>
2025-09-24 20:06:57 +0800

    🔒️ fix: enhance Firefox privacy settings and disable tracking
    - Add settings to disable extension recommendations and sponsored content
    - Disable Normandy system and Shield studies for better privacy
    - Add new anti-advertising settings for new tab page
    - Reorganize settings with improved grouping and comments
    - Strengthen telemetry and crash reporting controls

--- modules/home-manager/desktop/firefox.nix
+++ modules/home-manager/desktop/firefox.nix
@@ -35,29 +35,33 @@ in
         settings =
           let
             extensions = {
+              "extensions.htmlaboutaddons.recommendations.enabled" = false;
               "extensions.update.autoUpdateDefault" = false;
               "extensions.update.enabled" = false;
             };
           in
           {
-            # disable annoyinh Ctrl+Q shortcut
-            "browser.quitShortcut.disabled" = true;
             # don't mess up with paste
             "dom.event.clipboardevents.enabled" = false;
             # enable hw video acceleration, if supported
             "media.ffmpeg.vaapi.enabled" = true;
             # handpicked settings from: https://github.com/arkenfox/user.js/blob/master/user.js
             # ads
+            "app.shield.optoutstudies.enabled" = false;
+            "app.normandy.enabled" = false;
+
             "browser.newtabpage.activity-stream.showSponsored" = false;
             "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
-            "extensions.htmlaboutaddons.recommendations.enabled" = false;
+            "browser.newtabpage.activity-stream.showSponsoredCheckboxes" = false;
+            "browser.newtabpage.activity-stream.default.sites" = "";
+            # disable annoyinh Ctrl+Q shortcut
+            "browser.quitShortcut.disabled" = true;
+            "browser.tabs.crashReporting.sendReport" = false;
             "browser.urlbar.suggest.quicksuggest.sponsored" = false;
+
             # telemetry
             "datareporting.policy.dataSubmissionEnable" = false;
             "datareporting.healthreport.uploadEnabled" = false;
-            "app.shield.optoutstudies.enabled" = false;
-            "app.normandy.enabled" = false;
-            "browser.tabs.crashReporting.sendReport" = false;
           }
           // lib.optionalAttrs cfg.subpixelRender.enable {
             # https://pandasauce.org/get-fonts-done/
