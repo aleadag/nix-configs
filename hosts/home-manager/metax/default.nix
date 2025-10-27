f0360357116e1f9e4faef72d4b3da51ecab5b5ec
Alexander Wang <aleadag@gmail.com>
2025-10-27 16:18:32 +0800

    🐛 fix: set XDG_RUNTIME_DIR for systemd user services in SSH sessions

--- hosts/home-manager/metax/default.nix
+++ hosts/home-manager/metax/default.nix
@@ -4,6 +4,11 @@
     username = "alexander";
     homeDirectory = "/home/${username}";
     stateVersion = "25.05";
+
+    # Set XDG_RUNTIME_DIR for systemd user services in SSH sessions
+    sessionVariables = {
+      XDG_RUNTIME_DIR = "/run/user/1001";
+    };
   };
 
   home-manager = {
