92045c699c8eddccc86f2f8d891c9544d0ab3aa3
Alexander Wang <aleadag@gmail.com>
2025-10-27 14:25:12 +0800

    ✨ feat: add metax Home Manager configuration for Linux

---
+++ hosts/home-manager/metax/default.nix
@@ -0,0 +1,18 @@
+{
+  home = rec {
+    username = "alexander";
+    homeDirectory = "/home/${username}";
+    stateVersion = "25.05";
+  };
+
+  home-manager = {
+    dev.enable = true;
+    editor = {
+      enable = false;
+      neovim.enable = true;
+    };
+    gui.enable = false;
+  };
+
+  targets.genericLinux.enable = true;
+}
