f8c9b17662bbe038555d2ca41740e10e46ed2839
Alexander Wang <aleadag@gmail.com>
2026-01-15 02:47:34 -0800

    ♻️ refactor(im): replace fcitx5-chinese-addons with rime-ice
    Switch Chinese input method from pinyin to Rime with rime-ice schema:
    - Use fcitx5-rime with rime-ice, rime-moegirl, and rime-zhwiki data
    - Add rime custom configuration with rime_ice schema
    - Configure Shift keys to toggle Chinese/ASCII mode
    - Clean up redundant comments in behavior settings

--- modules/home-manager/desktop/im.nix
+++ modules/home-manager/desktop/im.nix
@@ -6,6 +6,14 @@
 }:
 let
   cfg = config.home-manager.desktop.im;
+  fcitx5-rime-with-data = pkgs.fcitx5-rime.override {
+    rimeDataPkgs = with pkgs; [
+      rime-data # base data with default.yaml, punctuation.yaml, etc.
+      rime-ice
+      rime-moegirl
+      rime-zhwiki
+    ];
+  };
 in
 {
   options.home-manager.desktop.im = {
@@ -19,14 +27,13 @@ in
       enable = true;
       type = "fcitx5";
       fcitx5 = {
-        addons = with pkgs.qt6Packages; [ fcitx5-chinese-addons ];
+        addons = [ fcitx5-rime-with-data ];
         waylandFrontend = config.home-manager.window-manager.wayland.enable;
         settings = {
           globalOptions = {
             Hotkey = {
               EnumerateWithTriggerKeys = true;
               EnumerateSkipFirst = false;
-              ModifierOnlyKeyTimeout = 250;
             };
             "Hotkey/EnumerateForwardKeys" = {
               "0" = "Super+space";
@@ -47,25 +54,15 @@ in
               "0" = "Tab";
             };
             Behavior = {
-              # Active By Default
               ActiveByDefault = false;
-              # Share Input State
               ShareInputState = "No";
-              # Show preedit in application
               PreeditEnabledByDefault = true;
-              # Show Input Method Information when switch input method
               ShowInputMethodInformation = true;
-              # Show Input Method Information when changing focus
               showInputMethodInformationWhenFocusIn = false;
-              # Show compact input method information
               CompactInputMethodInformation = true;
-              # Show first input method information
               ShowFirstInputMethodInformation = true;
-              # Default page size
-              DefaultPageSize = 5;
-              # Override Xkb Option
+              DefaultPageSize = 6;
               OverrideXkbOption = false;
-              # Preload input method to be used by default
               PreloadInputMethod = true;
             };
           };
@@ -75,14 +72,15 @@ in
             "Groups/0" = {
               Name = "Default";
               "Default Layout" = "us";
-              DefaultIM = "pinyin";
+              DefaultIM = "rime";
             };
             "Groups/0/Items/0".Name = "keyboard-us";
-            "Groups/0/Items/1".Name = "pinyin";
+            "Groups/0/Items/1".Name = "rime";
           };
 
-          addons.unicode = {
-            sections = {
+          addons = {
+            rime.globalSection.SwitchKey = "Control+F4";
+            unicode.sections = {
               TriggerKey."0" = "Control+Alt+Shift+U";
               DirectUnicodeMode."0" = "Shift+Super+U";
             };
@@ -90,5 +88,18 @@ in
         };
       };
     };
+
+    xdg.dataFile."fcitx5/rime/default.custom.yaml".text = # yaml
+      ''
+        patch:
+          schema_list:
+            - schema: rime_ice
+          menu:
+            page_size: 6
+          ascii_composer:
+            switch_key:
+              Shift_L: commit_code
+              Shift_R: commit_code
+      '';
   };
 }
