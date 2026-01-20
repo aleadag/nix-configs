49e8f14fcbedf8f148ea1de4dc1099f9050b37da
Alexander Wang <aleadag@gmail.com>
2026-01-20 20:47:19 +0800

    kanshi(mbx): update docked profile

--- modules/home-manager/window-manager/wayland/kanshi/mbx.nix
+++ modules/home-manager/window-manager/wayland/kanshi/mbx.nix
@@ -8,4 +8,18 @@
       }
     ];
   }
+  {
+    profile.name = "docked";
+    profile.outputs = [
+      {
+        criteria = "DP-2";
+        status = "enable";
+        scale = 1.5;
+      }
+      {
+        criteria = "eDP-1";
+        status = "disable";
+      }
+    ];
+  }
 ]
