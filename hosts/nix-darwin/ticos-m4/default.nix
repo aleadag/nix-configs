49d37640499c09f21a2c3878f0f98448afcbb4ac
Alexander Wang <aleadag@gmail.com>
2026-03-06 15:14:53 +0800

    ✨ feat: add new nix-darwin host ticos-m4

---
+++ hosts/nix-darwin/ticos-m4/default.nix
@@ -0,0 +1,26 @@
+{
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  nix-darwin.home = {
+    username = "alexander";
+    extraModules = {
+      home.stateVersion = "26.05";
+      home-manager = {
+        desktop.mpv.enable = false;
+        openclaw.enable = true;
+        syncthing.enable = false;
+        window-manager.paneru.enable = false;
+      };
+    };
+  };
+
+  nix-darwin = {
+    homebrew.enable = false;
+  };
+
+  # This value determines the nix-darwin release with which your system is to
+  # be compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after nix-darwin release notes say you
+  # should.
+  system.stateVersion = 6; # Did you read the comment?
+}
