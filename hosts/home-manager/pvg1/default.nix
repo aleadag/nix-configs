1a268356a81b2c4d86ccd11720d1de9c7b05b4ff
Alexander Wang <aleadag@gmail.com>
2026-03-14 19:15:43 +0800

    🔧 chore: configure window manager and wayland for pvg1

--- hosts/home-manager/pvg1/default.nix
+++ hosts/home-manager/pvg1/default.nix
@@ -7,13 +7,24 @@
 
   home-manager = {
     dev.enable = true;
-    editor = {
-      enable = false;
-      neovim.enable = true;
-    };
-    gui.enable = false;
+    desktop.enable = true;
+    mihomo.enable = false;
     nix.niks3.gc.enable = true;
+    window-manager = {
+      enable = true;
+      x11.enable = false;
+    };
   };
 
-  targets.genericLinux.enable = true;
+  targets.genericLinux = {
+    enable = true;
+    gpu = {
+      enable = true;
+      nvidia = {
+        enable = true;
+        version = "550.144.03";
+        sha256 = "sha256-akg44s2ybkwOBzZ6wNO895nVa1KG9o+iAb49PduIqsQ=";
+      };
+    };
+  };
 }
