11e9f5ebe740df89be1cbcc3dc766a40bf31ec7b
Alexander Wang <aleadag@gmail.com>
2026-03-27 10:26:16 +0800

    ♻️ refactor: centralize NixOS system defaults

--- modules/nixos/system/default.nix
+++ modules/nixos/system/default.nix
@@ -25,23 +25,51 @@ in
     motd.enable = lib.mkEnableOption "show message of the day" // {
       default = true;
     };
-    zram.enable = lib.mkEnableOption "page compression" // {
-      default = true;
+    pageCompression = {
+      enable = lib.mkOption {
+        description = "Page compression strategy.";
+        type = lib.types.enum [
+          "none"
+          "zram"
+          "zswap"
+        ];
+        default = "zswap";
+      };
+      algorithm = lib.mkOption {
+        description = "Page compression algorithm.";
+        type = lib.types.str;
+        default = "lzo";
+      };
+      memoryPercent = lib.mkOption {
+        description = "Maximum amount of memory (in percentage) that can be used.";
+        type = lib.types.int;
+        default = 25;
+      };
     };
   };
 
   config = lib.mkIf cfg.enable {
     boot = {
-      initrd.systemd.enable = lib.mkDefault true;
+      initrd = {
+        systemd.enable = lib.mkDefault true;
+      };
+
+      kernelParams = lib.mkIf (cfg.pageCompression.enable == "zswap") [
+        "zswap.compressor=${cfg.pageCompression.algorithm}"
+        "zswap.enabled=1"
+        "zswap.max_pool_percent=${toString cfg.pageCompression.memoryPercent}"
+      ];
 
       kernel.sysctl = {
         # Enable Magic keys
         "kernel.sysrq" = 1;
+      }
+      // lib.optionalAttrs (cfg.pageCompression.enable == "zram") {
         # https://wiki.archlinux.org/title/Zram#Optimizing_swap_on_zram
-        "vm.swappiness" = lib.mkIf cfg.zram.enable 180;
-        "vm.watermark_boost_factor" = lib.mkIf cfg.zram.enable 0;
-        "vm.watermark_scale_factor" = lib.mkIf cfg.zram.enable 125;
-        "vm.page-cluster" = lib.mkIf cfg.zram.enable 0;
+        "vm.swappiness" = 180;
+        "vm.watermark_boost_factor" = 0;
+        "vm.watermark_scale_factor" = 125;
+        "vm.page-cluster" = 0;
       };
 
       # Disable boot editor for security
@@ -72,14 +100,6 @@ in
         enable = true;
         interval = "weekly";
       };
-
-      zram-generator = {
-        inherit (cfg.zram) enable;
-        settings.zram0 = {
-          zram-size = lib.mkDefault "min(ram / 2, 4096)";
-          compression-algorithm = lib.mkDefault "zstd";
-        };
-      };
     };
 
     systemd = {
@@ -96,8 +116,13 @@ in
         enable = lib.mkDefault true;
         flake = "github:thiagokokada/nix-configs";
         persistent = true;
-        # Not enabled by default for desktop/laptops, see nixos.server
-        # allowReboot = true;
+        # Enabled by default only in servers
+        allowReboot = lib.mkDefault (config.device.type == "server");
+        rebootWindow = {
+          lower = lib.mkDefault "02:30";
+          upper = lib.mkDefault "05:30";
+        };
+        randomizedDelaySec = lib.mkDefault "30min";
       };
       configurationRevision = flake.rev or "dirty";
     };
@@ -106,5 +131,11 @@ in
     users.motd = lib.mkIf cfg.motd.enable ''
       Welcome to '${config.networking.hostName}' running NixOS ${config.system.nixos.version}!
     '';
+
+    # Enable zram to have better memory management
+    zramSwap = lib.mkIf (cfg.pageCompression.enable == "zram") {
+      enable = true;
+      inherit (cfg.pageCompression) algorithm memoryPercent;
+    };
   };
 }
