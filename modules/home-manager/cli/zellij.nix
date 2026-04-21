de3577c95e9f27c7bb33528640f98a0e5ff23b47
Alexander Wang <aleadag@gmail.com>
2026-04-21 12:00:03 +0800

    💄 style: enable zellij pane frames and rounded corners

--- modules/home-manager/cli/zellij.nix
+++ modules/home-manager/cli/zellij.nix
@@ -15,9 +15,21 @@
       settings = {
         default_mode = "normal";
         default_shell = lib.mkIf config.programs.zsh.enable (lib.getExe config.programs.zsh.package);
+        pane_frames = true;
+        ui = {
+          pane_frames = {
+            rounded_corners = true;
+          };
+        };
+        simplified_ui = false;
+        copy_on_select = true;
         show_startup_tips = false;
         show_release_notes = false;
       };
+    };
+
+    home.shellAliases = {
+      zj = "zellij";
     };
   };
 }
