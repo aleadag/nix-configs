20a3161fa0ef4ad3661f087a3eec2b77af2b3647
Alexander Wang <aleadag@gmail.com>
2026-04-27 23:53:43 +0800

    ♻️ refactor: use starship-jj for jj status

--- modules/home-manager/cli/starship.nix
+++ modules/home-manager/cli/starship.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 {
@@ -24,43 +25,16 @@
 
         # custom module for jj status
         custom.jj = {
+          command = "prompt";
+          format = "$output";
           ignore_timeout = true;
-          description = "The current jj status";
-          detect_folders = [ ".jj" ];
-          symbol = "🥋 ";
-          command = ''
-            jj log --revisions @ --no-graph --ignore-working-copy --color always --limit 1 --template '
-              separate(" ",
-                change_id.shortest(4),
-                bookmarks,
-                "|",
-                concat(
-                  if(conflict, "💥"),
-                  if(divergent, "🚧"),
-                  if(hidden, "👻"),
-                  if(immutable, "🔒"),
-                ),
-                raw_escape_sequence("\x1b[1;32m") ++ if(empty, "(empty)"),
-                raw_escape_sequence("\x1b[1;32m") ++ coalesce(
-                  truncate_end(29, description.first_line(), "…"),
-                  "(no description set)",
-                ) ++ raw_escape_sequence("\x1b[0m"),
-              )
-            '
-          '';
-        };
-
-        # optionally disable git modules
-        git_state.disabled = true;
-        git_commit.disabled = true;
-        git_metrics.disabled = true;
-        git_branch.disabled = true;
-
-        # re-enable git_branch as long as we're not in a jj repo
-        custom.git_branch = {
+          shell = [
+            (lib.getExe pkgs.starship-jj)
+            "--ignore-working-copy"
+            "starship"
+          ];
+          use_stdin = false;
           when = true;
-          command = "jj root >/dev/null 2>&1 || starship module git_branch";
-          description = "Only show git_branch if we're not in a jj repo";
         };
       };
     };
