d8cc9f3cdb277277e8fceff209229fb5f7e36c70
Alexander Wang <aleadag@gmail.com>
2026-06-01 15:32:33 +0800

    💄 style: use curved ToggleTerm borders

--- modules/home-manager/editor/neovim/default.nix
+++ modules/home-manager/editor/neovim/default.nix
@@ -752,6 +752,7 @@ in
                     require("toggleterm").setup {
                       direction = "float",
                       float_opts = {
+                        border = "curved",
                         width = function()
                           return math.floor(vim.o.columns * 0.9)
                         end,
