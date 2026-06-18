3094f7f9a50638eee33a0895662190bb40633c48
Alexander Wang <aleadag@gmail.com>
2026-06-18 18:07:39 +0800

    🐛 fix: use Bulletty macOS config path

--- modules/home-manager/cli/bulletty.nix
+++ modules/home-manager/cli/bulletty.nix
@@ -6,6 +6,11 @@
 }:
 let
   cfg = config.home-manager.cli.bulletty;
+  configPath =
+    if pkgs.stdenv.hostPlatform.isDarwin then
+      "Library/Application Support/bulletty/config.toml"
+    else
+      ".config/bulletty/config.toml";
 in
 {
   options.home-manager.cli.bulletty.enable = lib.mkEnableOption "bulletty config" // {
@@ -15,7 +20,7 @@ in
   config = lib.mkIf cfg.enable {
     home = {
       packages = [ pkgs.bulletty ];
-      file.".config/bulletty/config.toml".source = (pkgs.formats.toml { }).generate "bulletty" {
+      file.${configPath}.source = (pkgs.formats.toml { }).generate "bulletty" {
         datapath = "${config.home.homeDirectory}/Sync/bulletty";
       };
     };
