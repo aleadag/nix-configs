f838000167b073bf0444763c3def15cd85f597ff
Alexander Wang <aleadag@gmail.com>
2026-08-16 03:09:29 +0800

    🐛 fix: guard Darwin Nvim app activation
    Refs: nix-configs-xft

--- modules/home-manager/darwin/nvim-app.nix
+++ modules/home-manager/darwin/nvim-app.nix
@@ -111,7 +111,7 @@ in
     };
   };
 
-  config = lib.mkIf cfg.enable {
+  config = lib.mkIf (config.home-manager.darwin.enable && cfg.enable) {
     home.activation.installNvimApp = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       apps_dir="$HOME/Applications"
       mkdir -p "$apps_dir"
