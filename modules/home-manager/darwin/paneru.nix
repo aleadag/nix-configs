d2a47f995a9b957b294861813dd93d9c0ecf27da
Alexander Wang <aleadag@gmail.com>
2026-09-12 01:12:00 +0800

    ✨ feat(paneru): migrate settings to lua

--- modules/home-manager/darwin/paneru.nix
+++ modules/home-manager/darwin/paneru.nix
@@ -13,80 +13,85 @@ in
 
   config = lib.mkIf cfg.enable {
     services.paneru.enable = true;
-    services.paneru.settings = {
-      options = {
-        animation_speed = 50;
-        auto_center = true;
-        focus_follows_mouse = false;
-        mouse_follows_focus = false;
-        preset_column_widths = [
-          0.25
-          0.33
-          0.5
-          0.66
-          0.75
-        ];
-        virtual_workspace_animations = true;
-      };
+    services.paneru.config = # lua
+      ''
+        paneru.setup {
+          options = {
+            animation_speed = 50.0,
+            auto_center = true,
+            focus_follows_mouse = false,
+            mouse_follows_focus = false,
+            preset_column_widths = {
+              0.25,
+              0.33,
+              0.5,
+              0.66,
+              0.75,
+            },
+            virtual_workspace_animations = true,
+          },
 
-      bindings = {
-        window_focus_west = "cmd - h";
-        window_focus_east = "cmd - l";
-        window_focus_north = "cmd - k";
-        window_focus_south = "cmd - j";
-        window_swap_west = "cmd + shift - h";
-        window_swap_east = "cmd + shift - l";
-        window_swap_first = "cmd + shift - k";
-        window_swap_last = "cmd + shift - j";
-        window_focus_first = "cmd - home";
-        window_focus_last = "cmd - end";
-        window_virtualnum_1 = "cmd - q";
-        window_virtualnum_2 = "cmd - w";
-        window_virtualnum_3 = "cmd - e";
-        window_virtualnum_4 = "cmd - r";
-        window_virtualnum_5 = "cmd - t";
-        window_virtualnum_6 = "cmd - y";
-        window_virtualnum_7 = "cmd - u";
-        window_virtualnum_8 = "cmd - i";
-        window_virtualnum_9 = "cmd - o";
-        window_virtualnum_10 = "cmd - p";
-        window_virtualmovenum_1 = "cmd + shift - q";
-        window_virtualmovenum_2 = "cmd + shift - w";
-        window_virtualmovenum_3 = "cmd + shift - e";
-        window_virtualmovenum_4 = "cmd + shift - r";
-        window_virtualmovenum_5 = "cmd + shift - t";
-        window_virtualmovenum_6 = "cmd + shift - y";
-        window_virtualmovenum_7 = "cmd + shift - u";
-        window_virtualmovenum_8 = "cmd + shift - i";
-        window_virtualmovenum_9 = "cmd + shift - o";
-        window_virtualmovenum_10 = "cmd + shift - p";
-        window_nextdisplay = "cmd + ctrl + shift - n";
-        mouse_nextdisplay = "cmd + ctrl + shift - m";
-        window_center = "cmd + shift - c";
-        window_resize = "cmd - .";
-        window_fullwidth = "cmd + shift - m";
-        window_manage = "cmd + ctrl - t";
-        window_stack = "cmd - [";
-        window_unstack = "cmd - ]";
-        quit = "ctrl + alt - delete";
-      };
+          bindings = {
+            ["window focus west"] = "cmd - h",
+            ["window focus east"] = "cmd - l",
+            ["window focus north"] = "cmd - k",
+            ["window focus south"] = "cmd - j",
+            ["window swap west"] = "cmd + shift - h",
+            ["window swap east"] = "cmd + shift - l",
+            ["window swap first"] = "cmd + shift - k",
+            ["window swap last"] = "cmd + shift - j",
+            ["window focus first"] = "cmd - home",
+            ["window focus last"] = "cmd - end",
+            ["window virtualnum 1"] = "cmd - q",
+            ["window virtualnum 2"] = "cmd - w",
+            ["window virtualnum 3"] = "cmd - e",
+            ["window virtualnum 4"] = "cmd - r",
+            ["window virtualnum 5"] = "cmd - t",
+            ["window virtualnum 6"] = "cmd - y",
+            ["window virtualnum 7"] = "cmd - u",
+            ["window virtualnum 8"] = "cmd - i",
+            ["window virtualnum 9"] = "cmd - o",
+            ["window virtualnum 10"] = "cmd - p",
+            ["window virtualmovenum 1"] = "cmd + shift - q",
+            ["window virtualmovenum 2"] = "cmd + shift - w",
+            ["window virtualmovenum 3"] = "cmd + shift - e",
+            ["window virtualmovenum 4"] = "cmd + shift - r",
+            ["window virtualmovenum 5"] = "cmd + shift - t",
+            ["window virtualmovenum 6"] = "cmd + shift - y",
+            ["window virtualmovenum 7"] = "cmd + shift - u",
+            ["window virtualmovenum 8"] = "cmd + shift - i",
+            ["window virtualmovenum 9"] = "cmd + shift - o",
+            ["window virtualmovenum 10"] = "cmd + shift - p",
+            ["window nextdisplay"] = "cmd + ctrl + shift - n",
+            ["mouse nextdisplay"] = "cmd + ctrl + shift - m",
+            ["window center"] = "cmd + shift - c",
+            ["window resize"] = "cmd - .",
+            ["window fullwidth"] = "cmd + shift - m",
+            ["window manage"] = "cmd + ctrl - t",
+            ["window stack"] = "cmd - [",
+            ["window unstack"] = "cmd - ]",
+            ["quit"] = "ctrl + alt - delete",
+          },
 
-      decorations = {
-        active.border = {
-          enabled = true;
-          color = "#${config.lib.stylix.colors.base0D}";
-        };
-      };
+          decorations = {
+            active = {
+              border = {
+                enabled = true,
+                color = "#${config.lib.stylix.colors.base0D}",
+              },
+            },
+          },
 
-      windows = {
-        syspref = {
-          # Title RegExp pattern is required.
-          title = ".*";
-          bundle_id = "com.apple.systempreferences";
-          # Do not manage this window, e.g. it will be floating.
-          floating = true;
-        };
-      };
-    };
+          windows = {
+            syspref = {
+              -- Title RegExp pattern is required.
+              title = ".*",
+              bundle_id = "com.apple.systempreferences",
+              -- Do not manage this window, e.g. it will be floating.
+              floating = true,
+            },
+          },
+        }
+      '';
   };
 }
